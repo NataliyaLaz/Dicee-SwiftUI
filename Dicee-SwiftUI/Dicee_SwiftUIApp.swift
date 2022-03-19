e444e6c0a6b19ccdf5d82b0646dac171187b4cb3
NataliyaLaz <nataliya.laz@gmail.com>
2022-03-19 20:24:44 +0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Nataliya Lazouskaya on 19.03.22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
